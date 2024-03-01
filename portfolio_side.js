0a38f40d2b0b482f3254d563d7ff29b977fadc52
Mahamudul Islam Shawcha <shawcha2007040@stud.kuet.ac.bd>
2024-03-02 00:55:39 +0600

    commiting changes

--- portfolio_side.js
+++ portfolio_side.js
@@ -26,6 +26,7 @@ let mixerPortfolio = mixitup('.work_container', {
 document.addEventListener("click",(e)=>{
     if(e.target.classList.contains("work_button")){
         togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
     }
 })
 function togglePortfolioPopup()
@@ -33,3 +34,31 @@ function togglePortfolioPopup()
     document.querySelector(".portfolio_popup").classList.toggle("open");
 }
 document.querySelector(".portfolio_popup-close").addEventListener("click",togglePortfolioPopup);
+function portfolioItemDetails(portfolioItem)
+{
+    document.querySelector(".pp_thumbnail img").src= portfolioItem.querySelector(".work_img").src;
+    document.querySelector(".portfolio_popup-subtitle span").innerHTML= portfolioItem.querySelector(".work_title").innerHTML;
+    document.querySelector(".portfolio_popup-body").innerHTML= portfolioItem.querySelector("portfolio_item-details").innerHTML;
+
+}
+const modalViews = document.querySelectorAll('.services_modal'),
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalCloses = document.querySelectorAll('.services_modal-close');
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+modalCloses.forEach((modalclose)=>{
+    modalclose.addEventListener("click",()=>{
+        modalViews.forEach((modalView)=>
+        {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
